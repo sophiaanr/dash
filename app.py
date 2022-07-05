e7d7d0339aee8605913fb2d21626652a25fcb8cb
sophiaanr <snarita.r88@gmail.com>
2022-07-04 20:03:52 -0500

    delete components that don't work

--- app.py
+++ app.py
@@ -9,30 +9,3 @@ app = dash.Dash(__name__,
                 suppress_callback_exceptions=True)
 app.title = 'BlizEx'
 # favicon.ico only shows up if you navigate to http://127.0.0.1:8050/favicon.ico
-
-
-switch = html.Div([
-    daq.BooleanSwitch(id='my-boolean-switch', on=False),
-    html.Div(id='boolean-switch-output-1')
-])
-
-
-@app.callback(
-    Output('boolean-switch-output-1', 'children'),
-    Input('my-boolean-switch', 'on')
-)
-def update_output(on):
-    return 'The switch is {}.'.format(on)
-
-
-@app.callback(
-    Output('dark-theme-components-1', 'style'),
-    Input('toggle-theme', 'value'),
-    State('dark-theme-components-1', 'style')
-)
-def switch_bg(dark, currentStyle):
-    if dark:
-        currentStyle.update(backgroundColor='#303030')
-    else:
-        currentStyle.update(backgroundColor='white')
-    return currentStyle
