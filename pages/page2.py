91767fd11966976cbfea6c2d3cfb89bd6dc19639
sophiaanr <snarita.r88@gmail.com>
2022-07-25 14:24:44 -0500

    modify page 2 event selector

--- pages/page2.py
+++ pages/page2.py
@@ -1,35 +1,80 @@
 # Import necessary libraries
 import dash
-from dash import html
+from dash import html, Output, Input
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 
 ### Add the page components here
-table_header = [
-    html.Thead(html.Tr([html.Th("First Name"), html.Th("Last Name")]))
-]
+from app import app
 
-row1 = html.Tr([html.Td("Arthur"), html.Td("Dent")])
-row2 = html.Tr([html.Td("Ford"), html.Td("Prefect")])
-row3 = html.Tr([html.Td("Zaphod"), html.Td("Beeblebrox")])
-row4 = html.Tr([html.Td("Trillian"), html.Td("Astra")])
+radioitems = html.Div(
+    [
+        # dbc.Label("Choose one"),
+        dbc.RadioItems(
+            options=[
+                {"label": "Item 1", "value": 1},
+                {"label": "Item 2", "value": 2},
+                {"label": "Item 3", "value": 3},
+            ],
+            value=1,
+            id="radioitems-input",
+        ),
+    ]
+)
 
-table_body = [html.Tbody([row1, row2, row3, row4])]
+options = html.Div(
+    [
+        # dbc.Label("Choose one"),
+        dbc.RadioItems(
+            options=[
+                {"label": "Option 1", "value": 1},
+                {"label": "Option 2", "value": 2},
+                {"label": "Option 3", "value": 3},
+            ],
+            value=1,
+            id="options-input",
+        ),
+    ]
+)
 
-page2_table = dbc.Table(table_header + table_body, bordered=True)
+go_button = html.Div(
+    [
+        dbc.Row([dbc.Button('Go', size='sm', style={'width': '50%'})]),
+        dbc.Row(html.Br()),
+        dbc.Row(html.Br())
+    ]
+)
+
+table = dbc.Table([
+    #html.Thead(),
+    html.Tbody([
+        html.Tr([html.Th("Product"), html.Th("Options"), html.Th('View')]),
+        html.Tr(radioitems, style={'display': 'table-cell'}),
+        html.Tr(options, style={'display': 'table-cell'}),
+        html.Tr(go_button, style={'display': 'table-cell'})
+    ])
+], style={'display': 'table', 'width': '50%'})
 
 # Define the final page layout
 layout = dbc.Container([
     dbc.Row([
         html.Center(html.H1("Page 2")),
         html.Br(),
-        html.Hr(),
-        dbc.Col([
-            html.P("This is column 1."),
-            dbc.Button("Test Button", color="secondary")
-        ]),
-        dbc.Col([
-            html.P("This is column 2."),
-            page2_table
-        ])
+        html.Center(table)
+    ]),
+    html.Hr(),
+    dbc.Container([
+        html.H6('Product Description'),
+        html.P(id='prod-description')
     ])
 ])
+
+options = ['hi', 'hello', 'bye']
+
+
+@app.callback(
+    Output('prod-description', 'children'),
+    Input('radioitems-input', 'value')
+)
+def update(radio_val):
+    return options[radio_val - 1]
