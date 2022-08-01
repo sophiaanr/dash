a28efe54dadd3fc5fab4d098cc32bb90ed426e6a
sophiaanr <snarita.r88@gmail.com>
2022-08-01 17:08:07 -0500

    update layout

--- pages/MRRPro.py
+++ pages/MRRPro.py
@@ -2,11 +2,11 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import html
 
-dash.register_page(__name__, path='/MRRPro', title='MRRPro')
 
-layout = dbc.Container([
-    dbc.Row([
-        html.Center(html.H1("Micro Rain Radar (MRR)")),
-        html.Br(),
-    ])
-])
+def layout():
+    return dbc.Container([
+        dbc.Row([
+            html.Center(html.H1("Micro Rain Radar (MRR)")),
+            html.Br(),
+        ])
+    ], className='py-3')
